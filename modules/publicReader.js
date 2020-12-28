81354f549aefd6603934e1bd7582b26809252062
Degreet <degreetpro@gmail.com>
2020-12-28 18:22:02 +0200

    public reader

--- modules/publicReader.js
+++ modules/publicReader.js
@@ -1,7 +1,16 @@
 module.exports = async function readPublic() {
   const structure = await scanPublic()
-
+  return read("public", structure)
 }
 
 
-const scanPublic = require('./modules/publicScanner')
+const scanPublic = require('./publicScanner')
+const {readFile} = require("fs").promises
+
+
+async function read(path, struct) {
+  return Object.fromEntries(await Promise.all(Object.entries(struct).map(async ([name, item]) => [
+    name,
+    await (item === true ? readFile(`${path}/${name}`) : read(`${path}/${name}`, item))
+  ])))
+}
--- modules/publicScanner.js
+++ modules/publicScanner.js
@@ -7,7 +7,7 @@ const { readdir } = require("fs").promises
 
 
 async function scan(path) {
-  let info = await readdir(path).catch(({code}) => code=='ENOTDIR')
+  let info = await readdir(path).catch(({ code }) => code == 'ENOTDIR')
   return typeof info == 'boolean' ? info : Object.fromEntries(await
-    Promise.all(info.map(async name => [name, await scan(path+'/'+name)])))
+    Promise.all(info.map(async name => [name, await scan(path + '/' + name)])))
 }
