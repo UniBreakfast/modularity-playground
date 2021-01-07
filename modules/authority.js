a669f73af9d29ed0f214dccfe63573302004652b
unibreakfast <unibreakfast1@gmail.com>
2021-01-07 16:19:03 +0200

    add authority placeholder

---
+++ modules/authority.js
@@ -0,0 +1,17 @@
+const authority = {register, startSession, continueSession}
+
+
+export default authority
+
+
+function register(customId, password) {
+
+}
+
+function startSession(customId, password) {
+
+}
+
+function continueSession(token, id) {
+
+}
