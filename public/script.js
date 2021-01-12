ab6082b9d0ba23cbcf263bf8059de8170515941f
unibreakfast <unibreakfast1@gmail.com>
2021-01-12 23:35:15 +0200

    bacis Tabs class with constructor and render

---
+++ public/script.js
@@ -0,0 +1,7 @@
+const tabs = new Tabs(document.querySelector('tabs'), {
+  active: 4,
+  side: 'right',
+  id: 'myTabs',
+  className: 'tabs tabgroup',
+  classes: ['A', 'B']
+})
---
+++ public/tabs.js
@@ -0,0 +1,53 @@
+class Tabs {
+  constructor (tabs=[], {active=0, side='top', id, classes, className}={}) {
+    if (Array.isArray(tabs)) {
+      this.titles = tabs
+    } else if (tabs instanceof HTMLElement) {
+      this.tabs = tabs
+      this.titles = [...tabs.children].map(({title}, i) => title || 'tab '+i)
+    }
+    this.active = active
+    this.side = side
+    if (id) this.id = id
+    if (classes) this.classes = classes
+    if (className) this.className = className
+
+    this.render()
+  }
+
+  render() {
+    const tabgroup = document.createElement('tabgroup')
+    tabgroup.setAttribute('side', this.side)
+    tabgroup.innerHTML = `
+      <tabbuttons>
+        ${ this.titles.map(title => `<button>${title}</button>`).join('') }
+      </tabbuttons>
+      ${ this.tabs ? '' : `
+      <tabs>
+        ${ `<tab hidden></tab>`.repeat(this.titles.length) }
+      </tabs>
+      `}
+    `
+    if (this.tabs) {
+      this.tabs.replaceWith(tabgroup)
+      tabgroup.append(this.tabs)
+    }
+    if (this.id) tabgroup.id = this.id
+    if (this.className) {
+      tabgroup.className = this.className
+      delete this.className
+    }
+    if (this.classes) {
+      tabgroup.classList.add(...this.classes)
+      delete this.classes
+    }
+    const tabbuttons = [...tabgroup.children[0].children]
+    tabbuttons[this.active].disabled = true
+    const tabs = [...tabgroup.children[1].children]
+    tabs.forEach(tab => tab.removeAttribute('title'))
+    tabs[this.active].hidden = false
+    tabs.forEach((tab, i) => tab.hidden = this.active != i)
+
+    Object.assign(this, {tabgroup, tabbuttons, tabs})
+  }
+}
