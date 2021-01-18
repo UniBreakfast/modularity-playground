becf9e305c40f5299570dc86765f866cd931c0bb
unibreakfast <unibreakfast1@gmail.com>
2021-01-18 16:16:32 +0200

    layout corrections

--- public/authTesting/script.js
+++ public/authTesting/script.js
@@ -1,3 +1,12 @@
-const tabs = new Tabs(document.querySelector('tabs'), {
-  active: [3,4,5,6]
-})
+const tabs = new Tabs(document.querySelector('tabs'),
+  {active: [0,1,2]})
+const forms = new Tabs(document.querySelector('tab>tabs'),
+  {active: [0,1,2], side: "left", className: 'sideway'})
+const lines = new Tabs(document.querySelector('tab>tabs'),
+  {active: [0,1], side: "right", className: 'sideway'})
+const tables = new Tabs(document.querySelector('tab>tabs'),
+  {active: [0,1], side: "right", className: 'sideway'})
+
+tabs.shiftSplit(0, 30)
+tabs.shiftSplit(1, 63)
+lines.shiftSplit(0, -100)
