f633579724a74855004906371c1bad3402399592
unibreakfast <unibreakfast1@gmail.com>
2021-01-30 13:37:24 +0200

    refactor front part of pollers

---
+++ public/poller/pollers.js
@@ -0,0 +1,32 @@
+
+import {formPollerClass} from './poller.js'
+
+
+export const Poller = formPollerClass(generateId, request)
+
+
+async function request(id, {name}) {
+  await sleep(requestDelay)
+
+  try {
+    const response = await fetch('/poll',
+      {method: 'GET', headers: {'poll-id': id, 'poll-name': name}})
+    if (response.ok) return response.json()
+  } catch {}
+}
+const requestDelay = 1000
+
+
+function generateId() {
+  let token = ''
+  for (let i = 0; i < 13; ++i)
+    token += (i+1)%7 ? chars[random() * 62 | 0] : '-'
+  return token
+}
+const chars = '0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz'
+const {random} = Math
+
+
+function sleep(duration) {
+  return new Promise(resolve => setTimeout(resolve, duration))
+}
--- public/poller/testPolls.js
+++ public/poller/testPolls.js
@@ -1,5 +1,5 @@
 const pollMaster = require('./pollMaster')
-const {handle, broadcast, inform, informClient, informUser} = pollMaster
+const {polls, handle, broadcast, inform, informClient, informUser} = pollMaster
 
 
 require('http').createServer((req, resp) => {
@@ -14,9 +14,9 @@ else if (req.url == '/inform') {
   resp.end()
   informUser(req.headers.message, req.headers.name)
 }
-else if (req.url == '/poller.js') {
+else if (req.url.match(/\/pollers?\.js/)) {
   resp.setHeader('content-type', 'application/javascript')
-  require('fs').createReadStream('./public/poller/poller.js').pipe(resp)
+  require('fs').createReadStream('./public/poller'+req.url).pipe(resp)
 }
 
 
@@ -24,7 +24,7 @@ else if (req.url == '/poller.js') {
 else resp.end(/* html */`
 <script>
 
-import('/poller.js').then(({Poller}) => {
+import('/pollers.js').then(({Poller}) => {
 
   const poller =
     new Poller(document.title = prompt('Enter your name:', 'guest'))
@@ -44,4 +44,4 @@ import('/poller.js').then(({Poller}) => {
   () => console.log('Server started at http://localhost:3000'))
 
 
-Object.assign(global, {broadcast, informClient, informUser})
+Object.assign(global, {polls, broadcast, informClient, informUser})
