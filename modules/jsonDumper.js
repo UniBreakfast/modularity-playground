c28d129a27f172cad56a6c69cc9b8ca1ac1511fa
unibreakfast <unibreakfast1@gmail.com>
2021-02-10 19:56:41 +0200

    add list, discard, wipe to jsonDumper

--- modules/jsonDumper.js
+++ modules/jsonDumper.js
@@ -1,7 +1,8 @@
-module.exports = {save, load}
+module.exports = {save, load, list, discard, wipe}
 
 
-const {writeFile, readFile, mkdir} = require('fs/promises')
+const {writeFile, readFile, mkdir, readdir, stat, rm, rmdir} =
+  require('fs/promises')
 
 
 async function save(ref, subj, name) {
@@ -30,4 +31,43 @@ async function load(ref, subj, name) {
       .forEach(key => { if (!content.hasOwnProperty(key)) delete ref[key] })
     Object.assign(ref, content)
   } else throw new TypeError('Content type mismatch')
+}
+
+async function list(subj) {
+  if (subj) {
+    const list = []
+    if (await stat(`jsonDumps/${subj}.json`).catch(()=>{})) list.push(undefined)
+    const names = await readdir('jsonDumps/'+subj).catch(()=>{})
+    if (names) list.push(...names)
+    return list
+  } else {
+    const list = {}
+    if (await stat('dump.json').catch(()=>{})) list[''] = undefined
+    const subjects = await readdir('jsonDumps')
+    for (let subj of subjects) {
+      if (subj.endsWith('.json')) {
+        list[subj = subj.slice(0, -5)] = [undefined].concat(list[subj] || [])
+      } else {
+        const names = await readdir('jsonDumps/'+subj).catch(()=>{})
+        if (names) list[subj] = (list[subj] || []).concat(names)
+      }
+    }
+    return list
+  }
+}
+
+async function discard(subj, name) {
+  const path = name ? `jsonDumps/${subj}/${name}.json` : subj ?
+    `jsonDumps/${subj}.json` : 'dump.json'
+  await rm(path).catch(()=>{})
+}
+
+async function wipe(subj) {
+  if (subj) {
+    await rmdir("jsonDumps/"+subj, {recursive: true}).catch(()=>{})
+    await rm(`jsonDumps/${subj}.json`).catch(()=>{})
+  } else {
+    await rmdir('jsonDumps', {recursive: true}).catch(()=>{})
+    await rm('dump.json').catch(()=>{})
+  }
 }
